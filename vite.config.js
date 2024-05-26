86c2ed2a26d769721482c900fd885faa486a9cff
om50 <omchy34@gmail.com>
2024-05-26 17:01:06 +0530

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [reactRefresh(), react()],
   base: '/React-Weather-App/', // Ensure this matches your repository name
   build: {
     outDir: 'build',
