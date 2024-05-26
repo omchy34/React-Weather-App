1f8a4bd6e696e5d0a026baae1ef46d319f3f8048
om50 <omchy34@gmail.com>
2024-05-26 14:03:01 +0530

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Assuming your main React component is in App.jsx
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="React-Weather-App">
     <App />
-    </BrowserRouter>
   </React.StrictMode>,
+  document.getElementById('root')
 )
